81b1597eea29a6a596dc0e35903b78a833e9988c
Folkol <mattias4@kth.se>
2013-03-23 23:31:11 +0100

    Made a custom buffer, so that no blitting takes part in the video memory except from the final copy to videomemory. The last one is needed if we want the image to actually show up on screen

--- src/PoC.java
+++ src/PoC.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.Window;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
@@ -11,56 +14,81 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class PoC {
-    static int width = 800, height = 600;
-    static long lastTime = System.currentTimeMillis();
-    private static BufferedImage sprite;
+    int width = 800, height = 600;
+    long lastTime;
+    BufferedImage sprite, buffer;
 
     public static void main(String[] args) throws IOException {
-        Frame window = new Frame();
+        new PoC().run();
+    }
+
+    public void run() throws IOException {
+        Window window = createWindow();
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsConfiguration gc = ge.getDefaultScreenDevice().getDefaultConfiguration();
+
+        sprite = ImageIO.read(new File("sprite.bmp"));
+        buffer = gc.createCompatibleImage(width, height);
+
+        BufferStrategy bufferStrategy = window.getBufferStrategy();
+        while (true) {
+            drawPacmans();
+
+            Graphics drawGraphics = bufferStrategy.getDrawGraphics();
+            drawGraphics.clearRect(0, 0, width, height);
+            drawGraphics.drawImage(buffer, 0, 0, null);
+            drawGraphics.dispose();
+            bufferStrategy.show();
+        }
+
+    }
+
+    private Window createWindow() {
+        Window window = new Frame();
         window.setIgnoreRepaint(true);
         window.setVisible(true);
         window.createBufferStrategy(3);
         window.setSize(width, height);
 
         window.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent winEv) {
                 System.exit(0);
             }
         });
 
-        sprite = ImageIO.read(new File("sprite.bmp"));
-        BufferStrategy bufferStrategy = window.getBufferStrategy();
+        return window;
+    }
+
+    private void drawPacmans() {
         Random random = new Random();
-        while (true) {
-            long before = System.currentTimeMillis();
-            Graphics drawGraphics = bufferStrategy.getDrawGraphics();
 
-            drawGraphics.clearRect(0, 0, width, height);
-
-            drawPacmans(window, random, drawGraphics);
-            showFPS(before, drawGraphics, 30, 50);
-
-            drawGraphics.dispose();
-            bufferStrategy.show();
-        }
-    }
-
-    private static void drawPacmans(Frame window, Random random, Graphics drawGraphics) {
+        Graphics graphics = buffer.getGraphics();
         for (int i = 0; i < 1000; i++) {
-            final int x = random.nextInt(window.getWidth());
-            final int y = random.nextInt(window.getHeight());
-            drawGraphics.drawImage(sprite, x, y, null);
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            graphics.drawImage(sprite, x, y, null);
         }
+        showFPS(graphics);
+
+        graphics.dispose();
     }
 
-    private static void showFPS(long before, Graphics drawGraphics, int x, int y) {
-        drawGraphics.setColor(Color.WHITE);
-        drawGraphics.fillRect(x, y, 75, 20);
-        drawGraphics.setColor(Color.BLACK);
-        drawGraphics.drawRect(x, y, 75, 20);
+    private void showFPS(Graphics g) {
+        int x = 30;
+        int y = 50;
 
-        double elapsed = (System.currentTimeMillis() - before) / 1000.0;
-        if(elapsed == 0) elapsed = 1;
-        drawGraphics.drawString(String.format("FPS: %3.0f",  1.0 / elapsed), x + 15, y + 15);
+        double elapsed = (System.currentTimeMillis() - lastTime) / 1000.0;
+        if (elapsed == 0)
+            elapsed = 1;
+        double fps = 1.0 / elapsed;
+        lastTime = System.currentTimeMillis();
+
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, 75, 20);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, 75, 20);
+        g.drawString(String.format("FPS: %3.0f", fps), x + 15, y + 15);
     }
 }
